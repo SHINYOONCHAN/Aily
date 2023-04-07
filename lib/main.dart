e1e3157c37addc19a75c23a730bb48a8c80e2245
yoonchan <tlsdbscks123@gmail.com>
2023-04-07 22:41:17 +0900

    첫 번째 프로젝트

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ Future<void> main() async {
       create: (_) => UserProvider(),
       child: MultiProvider(
         providers: [
-
           ChangeNotifierProvider(create: (_) => TitleProvider()),
-
         ],
         child: const MaterialApp(
           home: LoginScreen(),
