5a7ddec7982275cf631cf5cabe9c497e3eca24d0
yoonchan <tlsdbscks123@gmail.com>
2023-04-12 15:56:20 +0900

    윤찬 : 홈 화면 테스트

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -241,6 +241,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               fontWeight: FontWeight.bold,
                             ),
                           ),
+
                           ElevatedButton(
                             onPressed: (){
                             },
